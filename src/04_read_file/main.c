869a7944d93300922376e0c915acb6b8d3eed2c9
guchiinu <guchiinu@gmail.com>
2020-12-05 12:05:46 +0900

    read_file追加

---
+++ src/04_read_file/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    read_file();
+
+    return 0;
+}
+
+void read_file()
+{
+    FILE *fp;
+    wchar_t text[100];
+    size_t size;
+
+    fp = fopen("resource/sample.txt","r");
+    if (fp == NULL)
+    {
+        printf("file not found.\n");
+        return ;
+    }
+    size = fread(text, sizeof(text), 100, fp);
+
+    printf("%ls\n", text);
+
+    return ;
+}
