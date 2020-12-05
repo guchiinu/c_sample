297778cf46b2778b42f146f8b3b727a36e31b1bc
guchi <guchi@ik1-315-17852.vs.sakura.ne.jp>
2020-12-05 14:52:36 +0900

    途中

--- src/04_read_file/main.c
+++ src/04_read_file/main.c
@@ -31,10 +31,38 @@ void read_file()
     printf("size: %d\n", size);
     printf("%s\n", text);
 
-    for (i = size - 2, j = 0; i >= 0; i=i-2, j++)
+    for (i = size - 3, j = 0; i >= 0; )
     {
+	printf("%x\n", text[i]);
+	if ( 0xFFFFFF00 <= text[i] && text[i] <= 0xFFFFFF7F )
+	{
+		printf("1バイト\n");
+		i-=1;
+		j+=1;
+	}
+	else if ( 0xFFFFFFC0 <= text[i] && text[i] <= 0xFFFFFFDF )
+        {
+                printf("2バイト\n");
+		i-=2;
+		j+=2;
+        }
+	else if ( 0xFFFFFFE0 <= text[i] && text[i] <= 0xFFFFFFFFEF )
+        {
+                printf("3バイト\n");
+		i-=3;
+		j+=3;
+        }
+	else
+	{
+		printf("?バイト\n");
+		// とりあえず１バイト進める
+		i-=1;
+		j+=1;
+	}
+
         rev_text[j] = text[i];
         rev_text[j+1] = text[i+1];
+	rev_text[j+2] = text[i+2];
     }
     rev_text[size] = '\0';
 
